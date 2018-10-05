08413c6efe661453d10ab8e9740db113821af5b0
Gaian <dinesh.kanna18@gmail.com>
2018-10-05 10:51:35 +0530

    First Commit Socratica Plus

---
+++ SocraticaPlus/SocraticaPlus/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SocraticaPlus
+//
+//  Created by Gaian on 05/10/18.
+//  Copyright © 2018 Gaian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
